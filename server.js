426211ac19cb5d2fce01d6a6901d9b96dc43f691
Srinidhi-4419 <kulkarnisrinidhi85@gmail.com>
2026-03-29 14:03:14 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send("Hello CI/CD"));
+app.get('/', (req, res) => {
+  res.send("Hello CI/CD");
+});
 
-app.listen(3000, () => console.log("Running on port 3000"));
-
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
+});
